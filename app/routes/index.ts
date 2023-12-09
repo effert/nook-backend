bf04a7873382b9d2e969cb348afd754bb35ec802
effert <effert111222@gmail.com>
2023-12-09 21:56:03 +0800

    。

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -10,12 +10,6 @@ import {
 const router = new Router()
 
 /**
- * 获取用户信息
- * @param id 用户id
- */
-router.get("/user-info", authMiddleware, getUserInfo)
-
-/**
  * 用户登录，未注册的话先注册再登录
  * @param email 邮箱
  * @param password 密码
@@ -26,4 +20,11 @@ router.post("/login", login)
  * 生成临时密码
  */
 router.get("/generate-temp-password", generateRandomPassword)
+
+/**
+ * 获取用户信息
+ * @param id 用户id
+ */
+router.get("/user-info", authMiddleware, getUserInfo)
+
 export default router
